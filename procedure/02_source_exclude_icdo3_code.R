85ab20f0dc85ec7a6262adcb4e72ddba2e987af7
Meera Patel <patelm9@mskcc.org>
2020-06-15 13:50:38 -0400

    update script from sandbox version

--- procedure/02_source_exclude_icdo3_code.R
+++ procedure/02_source_exclude_icdo3_code.R
@@ -1,200 +1,196 @@
 # Standard "exact" and "like" style searches after removing the icdo3 code from the label. For example "Meera Patel 9000/3" would have a "exact" and "like" search of "Meera Patel" for all labels that grepl TRUE for [0-9]{4}[/]{1}[0-9]{1}. If no sources contain that regex, all values will be NA.
 
+if (interactive()) {
 
-#rm(list = ls())
-#source('startup.R')
+                clean_env()
 
-path_to_output_fn <- paste0(stringr::str_replace(path_to_input_fn, "(^.*?[/]{1})(.*?)([.]{1}csv$)", "output/\\2_"), cave::strip_fn(cave::present_script_path()), ".csv")
+                source("startup.R")
 
-if (file.exists(path_to_output_fn)) {
-        secretary::typewrite_warning(path_to_output_fn, "already exists and will be overwritten.")
-        secretary::press_enter()
-}
+                # Temporary stop if the output file exists
+                brake_if_output_exists()
 
 
-input2 <- read_workfile(routine = "01_search_source")
-target_col <- source_col
+                # Read input
+                input <- read_input()
+                target_col <- source_col
 
-# Search Settings
-types <- c("exact", "like") # Can be either or both of c("exact", "like"), but "like" is not advised because the return can be a massive number of rows
-
-# Add rowid
-input3 <-
-        input2 %>%
-        #tibble::rowid_to_column("routine_id") %>%
-        dplyr::mutate_all(as.character)
-
-# Normalize NAs because some are "NA" and others are true NA
-input4 <-
-        input3 %>%
-        dplyr::mutate_all(stringr::str_replace_all, "^NA$", "") %>%
-        dplyr::mutate_all(na_if, "")
-
-# Creating final input object to join with final output object
-final_input <- input4
-
-# Parse the vectors that are strings
-input_vector <-
-        input4 %>%
-        dplyr::select(all_of(target_col)) %>%
-        unlist() %>%
-        #purrr::map(cave::string_to_vector) %>%
-        purrr::set_names(input4$routine_id)
-
-input_fact_concept <-
-        input4 %>%
-        dplyr::select(!!terminal_col) %>%
-        unlist()
-
-input_routine_id <- input4$routine_id
-
-qa1 <- length(input_fact_concept)-length(input_vector)
-
-if (qa1 != 0) {
-        stop("x and y are not equal in length.")
-}
+                # Routine Variables
+                types <- c("exact", "like")
 
 
-# Creating final_output object: named list by search type
-final_output <- list()
-for (i in 1:length(types)) {
-        final_output[[i]] <- tibble()
-}
-names(final_output) <- types
+                # Normalize NAs because some are "NA" and others are true NA
+                input2 <-
+                        input %>%
+                        normalize_na()
+
+                # Creating final input object to join with final output object
+                final_input <- input2
+
+                # Parse the vectors that are strings
+                input3 <-
+                        input2 %>%
+                        dplyr::select(routine_id, all_of(target_col), !!terminal_col)
 
 
-while (length(types) > 0) {
-        type <- types[1]
-output <- list()
-for (i in 1:length(input_vector)) {
+                # If any routine_id is NA
+                qa1 <-  input3$routine_id[is.na(input3$routine_id)]
 
-        if (!is.logical(input_vector[[i]]) && !(input_vector[[i]] %in% c(NA, "NA"))) {
+                if (length(qa1) > 0) {
 
-                        if (is.na(input_fact_concept[i])) {
+                        stop("routine_ids present in input are missing in input3")
 
-                                if (nchar(input_vector[[i]]) > source_skip_nchar) {
-                                        #print(i)
-                                        input_phrase <- input_vector[[i]]
-
-                                        if (grepl(icdo3_code_pattern_regex, input_phrase) == TRUE) {
-                                                print(input_phrase)
-                                        input_phrase <- stringr::str_replace_all(input_phrase, paste0("(^.* )(", icdo3_code_pattern_regex,")([ ]{0,}.*$)"), "\\1\\3") %>%
-                                                centipede::trimws(which = "both") %>%
-                                                centipede::no_blank()
-
-                                        if (length(input_phrase)) {
-                                        print(input_phrase)
-
-                                output[[i]] <-
-                                        chariot::query_phrase(input_phrase, type = type) %>%
-                                        filter_for_settings() %>%
-                                        # dplyr::mutate_at(vars(concept_name), function(x) str_replace_all(x, "[,]{1} ", " ")) %>%
-                                        rubix::arrange_by_nchar(concept_name) %>%
-                                        #filtering for only 250 lines (max in Excel)
-                                        dplyr::slice(1:250)
-                                        } else {
-                                                output[[i]] <- NA
-                                        }
-
-                                #names(output[[i]]) <- input_vector[[i]]
-                                        } else {
-                                                output[[i]] <- NA
-                                        }
-                                } else {
-                                        output[[i]] <- NA
-                                }
-
-
-
-                        } else {
-                                output[[i]] <- NA
-                        }
-
-        } else {
-                output[[i]] <- NA
-        }
-
-        names(output)[i] <- input_routine_id[i]
-}
-
-
-# Binding all search results with the search term preserved
-for (i in 1:length(output)) {
-
-        if (!is.logical(output[[i]])) {
-
-                if (type %in% c("exact")) {
-                        new_col_name <- "Source Remove ICDO3 Code Exact"
-                } else {
-                        new_col_name <- "Source Remove ICDO3 Code Like"
                 }
 
-                output[[i]] <- dplyr::bind_rows(output[[i]],
-                                                .id = "Source") %>%
-                                        chariot::merge_concepts(into = `Concept`) %>%
-                                dplyr::select(!!new_col_name := `Concept`)
 
+                final_output <- create_types_output_object()
+
+                while (length(types) > 0) {
+                        type <- types[1]
+                        output <- list()
+
+                        new_col_name <- paste0("Source Exclude ICDO3 Code", centipede::in_title_format(type))
+
+                        secretary::typewrite("Starting", type, "search.")
+                        Sys.sleep(1)
+                        cat("\n")
+
+
+                        for (i in 1:nrow(input3)) {
+
+                                        input_row <- input3 %>%
+                                                        dplyr::filter(row_number() == i)
+
+
+                                        rubix::release_df(input_row)
+
+
+                                        input_concept <- get(target_col)
+                                        output_concept <- get(terminal_col)
+                                        input_routine_id <- routine_id
+
+
+                                        if (!is.logical(input_concept) && !(input_concept %in% c(NA, "NA"))) {
+
+                                                        if (is.na(output_concept)) {
+
+                                                                if (nchar(input_concept) > source_skip_nchar) {
+
+                                                                        if (grepl(icdo3_code_pattern_regex, input_concept)) {
+                                                                                secretary::typewrite(crayon::bold("Concept with ICDO3 Code:"), input_concept)
+                                                                                input_concept <-
+                                                                                stringr::str_replace_all(input_concept, paste0("(^.* )(", icdo3_code_pattern_regex,")([ ]{0,}.*$)"), "\\1\\3") %>%
+                                                                                        centipede::trimws(which = "both") %>%
+                                                                                        centipede::no_blank()
+
+                                                                                if (nchar(input_concept) > source_skip_nchar) {
+                                                                                        secretary::typewrite(crayon::bold("Concept without ICDO3 Code:"), input_concept)
+
+                                                                                        output[[i]] <-
+                                                                                                query_phrase_in_athena(phrase = input_concept, type = type) %>%
+                                                                                                chariot::merge_concepts(into = `Concept`) %>%
+                                                                                                dplyr::select(!!new_col_name := `Concept`)
+                                                                                } else {
+
+
+                                                                                        output[[i]] <- NA
+                                                                                        output[[i]] <-
+                                                                                                output[[i]] %>%
+                                                                                                rubix::vector_to_tibble(!!new_col_name)
+                                                                                }
+
+                                                                        } else {
+
+                                                                                output[[i]] <- NA
+                                                                                output[[i]] <-
+                                                                                        output[[i]] %>%
+                                                                                        rubix::vector_to_tibble(!!new_col_name)
+
+                                                                        }
+
+
+                                                                } else {
+                                                                        output[[i]] <- NA
+                                                                        output[[i]] <-
+                                                                                output[[i]] %>%
+                                                                                rubix::vector_to_tibble(!!new_col_name)
+                                                                }
+
+
+
+                                                        } else {
+                                                                output[[i]] <- NA
+                                                                output[[i]] <-
+                                                                        output[[i]] %>%
+                                                                        rubix::vector_to_tibble(!!new_col_name)
+                                                        }
+
+                                        } else {
+                                                output[[i]] <- NA
+                                                output[[i]] <-
+                                                        output[[i]] %>%
+                                                        rubix::vector_to_tibble(!!new_col_name)
+                                        }
+
+                                        names(output)[i] <- input_routine_id
+
+                                        typewrite_progress(i = i, input3)
+                                        rm(list = colnames(input_row))
+                                        rm(input_row)
+                        }
+
+                        final_output[[type]] <- output %>%
+                                                dplyr::bind_rows(.id = "routine_id")
+
+                        #rm(output)
+                        types <- types[-1]
+                        cat("\n\n")
+                }
+
+        # Aggregating the search result columns to the original routine_id
+        final_output2 <-
+                final_output %>%
+                rubix::map_names_set(function(x) x %>%
+                                                        rubix::group_by_unique_aggregate(routine_id,
+                                                                                         agg.col = contains("Source"),
+                                                                                         collapse = "\n")) %>%
+                purrr::map(function(x) x %>%
+                                                                dplyr::mutate_at(vars(!routine_id), substr, 1, 25000))
+
+        # # If the search type is both exact and like, would need to reduce the list with left_join so each routine_id will have both searches associated with it in the dataframe
+
+        if (length(final_output2) > 1) {
+                final_output3 <-
+                        final_output2  %>%
+                        purrr::reduce(full_join, by = "routine_id")
+        } else {
+                final_output3 <-
+                        final_output2
         }
-}
 
 
-# For concepts that are already mapped or do not have an entry in the target column, creating a blank entry for the new columns
-for (i in 1:length(output)) {
+        # Join with final_input object
+        final_routine_output <-
+                dplyr::left_join(final_input,
+                                 final_output3)
 
-        if (is.logical(output[[i]])) {
 
-                        output[[i]] <-
-                                        rubix::vector_to_tibble(output[[i]],
-                                                                !!new_col_name)
-
+        #QA
+        qa2 <- all(final_routine_output$routine_id %in% final_input$routine_id)
+        if (qa2 == FALSE) {
+                stop("all routine_ids from final_input not in final_routine_output")
         }
+
+        qa3 <- nrow(final_routine_output) - nrow(final_input)
+        if (qa3 != 0) {
+                stop("row counts between final_input and final_routine_output don't match")
+        }
+
+
+        broca::simply_write_csv(x = final_routine_output,
+                                file = path_to_output_fn)
+
+} else {
+
+        source("/Users/patelm9/GitHub/omop_mapping/procedure/startup.R")
+
 }
-
-# Binding back to routine_id
-output <-
-        output %>%
-        dplyr::bind_rows(.id = "routine_id")
-
-# Adding to final output
-final_output[[type]] <- output %>%
-                                dplyr::distinct()
-
-# Removing first type
-types <- types[-1]
-}
-
-# Aggregating the search result columns to the original routine_id
-final_output2 <-
-        final_output %>%
-        rubix::map_names_set(function(x) x %>%
-                                                rubix::group_by_unique_aggregate(routine_id,
-                                                                                 agg.col = contains("Source"),
-                                                                                 collapse = "\n")) %>%
-        purrr::map(function(x) x %>%
-                                                        dplyr::mutate_at(vars(!routine_id), substr, 1, 25000))
-
-# If the search type is both exact and like, would need to reduce the list with left_join so each routine_id will have both searches associated with it in the dataframe
-final_output3 <-
-        final_output2  %>%
-        purrr::reduce(left_join)
-
-
-# Join with final_input object
-final_routine_output <-
-        dplyr::left_join(final_input,
-                         final_output3)
-
-#QA
-qa2 <- all(final_routine_output$routine_id %in% final_input$routine_id)
-if (qa2 == FALSE) {
-        stop("all routine_ids from final_input not in final_routine_output")
-}
-
-qa3 <- nrow(final_routine_output) - nrow(final_input)
-if (qa3 != 0) {
-        stop("row counts between final_input and final_routine_output don't match")
-}
-
-
-broca::simply_write_csv(x = final_routine_output,
-                        file = path_to_output_fn)
