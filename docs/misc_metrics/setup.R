4f677ab7d38d582cfcb659c684d0c57188bc22f7
Meera Patel <patelm9@mskcc.org>
2020-08-10 13:42:53 -0400

    update docs

--- docs/misc_metrics/setup.R
+++
@@ -1,7 +0,0 @@
-if ("/Users/patelm9/GitHub/KMI/termite/Map_to_OMOP/misc_metrics" != getwd()) {
-        setwd("/Users/patelm9/GitHub/KMI/termite/Map_to_OMOP/misc_metrics")
-}
-
-source('utils.R')
-
-path_to_input_dir <- "input/COVID"
